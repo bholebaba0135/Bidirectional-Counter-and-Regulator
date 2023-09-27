23ebbba4c5a2dfa3824636a19d6b16896f671e27
bholebaba0135 <anmol.gupta2357@gmail.com>
2023-09-27 15:33:38 +0530

    uploading code to githb

---
+++ sketch_apr12b.ino
@@ -0,0 +1,14 @@
+extern "C"
+{
+  void project();
+}
+void setup() {
+  // put your setup code here, to run once:
+  
+ project();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
